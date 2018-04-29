b02c6f789f5ba22c00eef1770433d13d2e664aa1
cheong <hwa1130k@gmail.com>
2018-04-30 02:23:14 +0900

    scanf-> cin printf -> cout

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,6 @@
-﻿#include <cstdio>
+﻿#include <iostream>
+
+using namespace std;
 
 #define MAP_WIDTH_SIZE 7
 #define MAP_HEIGHT_SIZE 6
@@ -21,7 +23,7 @@ int heuristic_table[MAP_HEIGHT_SIZE + 1][MAP_WIDTH_SIZE + 1] = {{ 3, 4, 5, 7, 5,
 
 
 void print_intro();													// 최초 실행 시 나타나는
-
+void select_first_player();											// 선공 결정
 void select_play_method();											// 1.search algorithm	2.rule base		3.직접 입력
 
 void compute_by_search_algorithm();									// search algorithm 을 통한 착수점 계산
@@ -34,70 +36,112 @@ void push_stone(char input_stone_location);							//
 
 void print_current_map();
 
+void play_connect4();
+
+
 
 int main() {
 
 	print_intro();
 	// 선/후공 결정 구현 필요
 	// 현재 무조건 흑이 선공
-	do {
+//	select_first_player();
+	play_connect4();
+/*	do {
 		select_play_method();
 		print_current_map();
-	} while (true);
+	} while (true);*/
 
 	return 0;
 }
 
 void print_intro() {																// 최초 실행 시 나타나는 안내문구
-	printf("**************************************************************\n");
-	printf("**************************Connect 4***************************\n");
-	printf("**************************************************************\n");
-	printf("*********Copyright by 아버지날보고있다면정답을알려조**********\n");
-	printf("**************************************************************\n");
-	printf("****************************************2018.04.29************\n");
-	printf("**************************************************************\n");
+	cout << "**************************************************************\n";
+//	printf("**************************************************************\n");
+	cout << "**************************Connect 4***************************\n";
+//	printf("**************************Connect 4***************************\n");
+	cout << "**************************************************************\n";
+//	printf("**************************************************************\n");
+	cout << "*********Copyright by 아버지날보고있다면정답을알려조**********\n";
+//	printf("*********Copyright by 아버지날보고있다면정답을알려조**********\n");
+	cout << "**************************************************************\n";
+//	printf("**************************************************************\n");
+	cout << "****************************************2018.04.29************\n";
+//	printf("****************************************2018.04.29************\n");
+	cout << "**************************************************************\n";
+//	printf("**************************************************************\n");
 
 
 }
 
+void select_first_player() {
+	bool is_input_correct = false;
+	char input_first_player;
+	do {
+		cout << "선공을 결정해주세요. (1. Computer 2. 사람) : ";
+		cin >> input_first_player;
+		if (input_first_player == '1') {
+			cout << "컴퓨터(○)의 선공입니다." << endl;
+			is_input_correct = true;
+		}
+		else if (input_first_player == '2') {
+			cout << "사람(○)의 선공입니다." << endl;
+			is_input_correct = true;
+		}
+		else {
+			cout << "잘못된 입력입니다. 다시 입력해주세요." << endl;
+		}
+	} while (!is_input_correct);
+}
+
 void select_play_method() {															// 작수 방식 결정
-	printf("\n");
+	cout << endl;
+//	printf("\n");
 	bool is_select_finished = false;
 	do {
-		printf("착수 방식을 선택해주세요. (1. Search Algorithm, 2. Rule Base, 3. 직접 입력)\n");
+		cout << "착수 방식을 선택해주세요. (1. Search Algorithm, 2. Rule Base, 3. 직접 입력)" << endl;
+//		printf("착수 방식을 선택해주세요. (1. Search Algorithm, 2. Rule Base, 3. 직접 입력)\n");
 		char input_play_method;
-		scanf("%s", &input_play_method);
+		cin >> input_play_method;
+//		scanf("%s", &input_play_method);
 		if (input_play_method == '1') {
-			printf("Search Algorithm에 기반하여 착수합니다.\n");
+			cout << "Search Algorithm에 기반하여 착수합니다." << endl;
+//			printf("Search Algorithm에 기반하여 착수합니다.\n");
 			
 			compute_by_search_algorithm();
 
 			is_select_finished = true;
 		}
 		else if (input_play_method == '2') {
-			printf("Rule Base로 착수합니다.\n");
+			cout << "Rule Base로 착수합니다." << endl;
+//			printf("Rule Base로 착수합니다.\n");
 
 			compute_by_rule_base();
 
 			is_select_finished = true;
 		}
 		else if (input_play_method == '3') {
-			printf("착수할 지점의 번호(1~7)를 입력해주세요.\n");
-//			getchar();
+			cout << "착수할 지점의 번호(1~7)를 입력해주세요." << endl;
+//			printf("착수할 지점의 번호(1~7)를 입력해주세요.\n");
+			getchar();
 			char input_stone_location;
 			do {
-				printf("착수 지점 : ");
-				scanf("%c", &input_stone_location);
+				cout << "착수 지점 : ";
+//				printf("착수 지점 : ");
+				cin >> input_stone_location;
+//				scanf("%c", &input_stone_location);
 				if (input_stone_location >= '1' && input_stone_location <= '7') {
 					is_select_finished = true;
 				}
 				else {
-					printf("착수 지점을 잘못 입력하였습니다. 다시 입력해주세요.\n");
+					cout << "착수 지점을 잘못 입력하였습니다. 다시 입력해주세요." << endl;
+//					printf("착수 지점을 잘못 입력하였습니다. 다시 입력해주세요.\n");
 				}
 			} while (!is_select_finished);
 			bool push_complete = check_possible_position(input_stone_location-'0');			// 입력 받은 열에 돌을 놓는다.
 			if (!push_complete) {											// 입력 받은 열에 돌을 놓을 수 없다.
-				printf("해당 지점은 돌이 가득 차 있습니다. 다시 입력해주세요.\n");
+				cout << "해당 지점은 돌이 가득 차 있습니다.다시 입력해주세요." << endl;
+//				printf("해당 지점은 돌이 가득 차 있습니다. 다시 입력해주세요.\n");
 				is_select_finished = false;
 			}
 			else {
@@ -105,7 +149,8 @@ void select_play_method() {															// 작수 방식 결정
 			}
 		}
 		else {
-			printf("잘못된 입력입니다. 다시 입력해주세요.\n\n");
+			cout << "잘못된 입력입니다. 다시 입력해주세요." << endl;
+//			printf("잘못된 입력입니다. 다시 입력해주세요.\n\n");
 		}
 	} while (!is_select_finished);
 	
@@ -194,4 +239,11 @@ int negamax(int depth) {
 
 	}
 	
+}
+
+void play_connect4() {
+	do {
+		select_play_method();
+		print_current_map();
+	} while (true);
 }
